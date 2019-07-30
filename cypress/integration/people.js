3f3a4693164a039c0338d6d891572c8175cec289
Vitor Cardoso <vitor.cardoso@ymail.com>
2019-07-29 22:28:13 -0300

    Change project structure

--- cypress/integration/people.js
+++ cypress/integration/people.js
@@ -17,86 +17,16 @@ describe('Star Wars People Response 200', () => {
    it('Get Star Wars people Response 200', () => {
       cy.request(baseURLPeople)
       .then((response) => {
+         expect(people.name).to.be.equal("Luke Skywalker")
+         expect(people.height).to.be.equal(172)
+         expect(people.mass).to.be.equal(77)
+         expect(people.hair_color).to.be.equal("blond")
+         expect(people.skin_color).to.be.equal("fair")
+         expect(people.eye_color).to.be.equal("blue")
+         expect(people.birth_year).to.be.equal("19BBY")
+         expect(people.gender).to.be.equal("male")
          expect(200).to.be.equal(200)
        })
     })
 })
 
-
-describe('Star Wars People name with ID 1', () => {
-    it('Get Star Wars people name with ID 1', () => {
-       cy.request(baseURLPeople)
-       .then((response) => {
-          expect(people.name).to.be.equal("Luke Skywalker")
-      })
-     })
- })
-
- describe('Star Wars People height with ID 1', () => {
-    it('Get Star Wars people height with ID 1', () => {
-      cy.request(baseURLPeople)
-       .then((response) => {
-         expect(people.height).to.be.equal(172)
-        })
-     })
- })
-
- describe('Star Wars People mass with ID 1', () => {
-   it('Get Star Wars people mass with ID 1', () => {
-      cy.request(baseURLPeople)
-      .then((response) => {
-        expect(people.mass).to.be.equal(77)
-       })
-    })
-})
-
-
-describe('Star Wars People hair color with ID 1', () => {
-   it('Get Star Wars people hair color with ID 1', () => {
-      cy.request(baseURLPeople)
-      .then((response) => {
-        expect(people.hair_color).to.be.equal("blond")
-       })
-    })
-})
-
-
-
-describe('Star Wars People skin color with ID 1', () => {
-   it('Get Star Wars people skin color with ID 1', () => {
-      cy.request(baseURLPeople)
-      .then((response) => {
-        expect(people.skin_color).to.be.equal("fair")
-       })
-    })
-})
-
-
-describe('Star Wars People eye color with ID 1', () => {
-   it('Get Star Wars people eye color with ID 1', () => {
-      cy.request(baseURLPeople)
-      .then((response) => {
-        expect(people.eye_color).to.be.equal("blue")
-       })
-    })
-})
-
-
-describe('Star Wars People birth year with ID 1', () => {
-   it('Get Star Wars people birth year with ID 1', () => {
-      cy.request(baseURLPeople)
-      .then((response) => {
-        expect(people.birth_year).to.be.equal("19BBY")
-       })
-    })
-})
-
-
-describe('Star Wars People gender with ID 1', () => {
-   it('Get Star Wars people gender with ID 1', () => {
-      cy.request(baseURLPeople)
-      .then((response) => {
-        expect(people.gender).to.be.equal("male")
-       })
-    })
-})
--- cypress/integration/planet.js
+++ cypress/integration/planet.js
@@ -18,93 +18,16 @@ var planet_informations = {
     it('Get Star Wars Planet Response 200', () => {
        cy.request(baseURLPlanet)
        .then((response) => {
-          expect(200).to.be.equal(200)
-        })
-     })
- })
- 
- 
- describe('Star Wars Planet name with ID 1', () => {
-     it('Get Star Wars Planet name with ID 1', () => {
-        cy.request(baseURLPlanet)
-        .then((response) => {
-           expect(planet.name).to.be.equal("Tatooine")
-       })
-      })
-  })
- 
-  describe('Star Wars Planet Rotation Period with ID 1', () => {
-     it('Get Star Wars Planet Rotation Period with ID 1', () => {
-        cy.request(baseURLPlanet)
-        .then((response) => {
-          expect(planet.rotation_period).to.be.equal(23)
-         })
-      })
-  })
- 
-  describe('Star Wars Planet orbital period with ID 1', () => {
-    it('Get Star Wars Planet orbital period with ID 1', () => {
-        cy.request(baseURLPlanet)
-       .then((response) => {
-         expect(planet.orbital_period).to.be.equal(304)
-        })
-     })
- })
- 
- 
- describe('Star Wars Planet diameter with ID 1', () => {
-    it('Get Star Wars Planet diameter with ID 1', () => {
-        cy.request(baseURLPlanet)
-       .then((response) => {
-         expect(planet.diameter).to.be.equal(10465)
-        })
-     })
- })
- 
- describe('Star Wars Planet climate with ID 1', () => {
-    it('Get Star Wars Planet climate with ID 1', () => {
-        cy.request(baseURLPlanet)
-       .then((response) => {
-         expect(planet.climate).to.be.equal("arid")
-        })
-     })
- })
- 
- describe('Star Wars Planet gravity with ID 1', () => {
-    it('Get Star Wars Planet gravity with ID 1', () => {
-        cy.request(baseURLPlanet)
-       .then((response) => {
-         expect(planet.gravity).to.be.equal("1 standard")
-        })
-     })
- })
- 
- 
- describe('Star Wars Planet terrain with ID 1', () => {
-    it('Get Star Wars Planet terrain with ID 1', () => {
-        cy.request(baseURLPlanet)
-       .then((response) => {
-         expect(planet.terrain).to.be.equal("desert")
-        })
-     })
- })
- 
- 
- describe('Star Wars Planet surface water  with ID 1', () => {
-    it('Get Star Wars Planet surface water with ID 1', () => {
-        cy.request(baseURLPlanet)
-       .then((response) => {
-         expect(planet.surface_water).to.be.equal(1)
-        })
-     })
- })
- 
- 
- describe('Star Wars Planet population with ID 1', () => {
-    it('Get Star Wars Planet population with ID 1', () => {
-        cy.request(baseURLPlanet)
-       .then((response) => {
-         expect(planet.population).to.be.equal(200000)
+            expect(planet.name).to.be.equal("Tatooine")
+            expect(planet.rotation_period).to.be.equal(23)
+            expect(planet.orbital_period).to.be.equal(304)
+            expect(planet.diameter).to.be.equal(10465)
+            expect(planet.climate).to.be.equal("arid")
+            expect(planet.gravity).to.be.equal("1 standard")
+            expect(planet.terrain).to.be.equal("desert")
+            expect(planet.surface_water).to.be.equal(1)
+            expect(planet.population).to.be.equal(200000)
+            expect(200).to.be.equal(200)
         })
      })
  })
